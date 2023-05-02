1f5e3eec5cae3edf0951718159ae1860c25d09c1
bhagysreedas <bhagyasreedas888@gmail.com>
2023-05-02 19:27:24 +0530

    2nd commit

--- store/urls.py
+++ store/urls.py
@@ -3,24 +3,30 @@ from store.views import *
 
 urlpatterns=[
    
-    path('',home,name = 'store'),
-    path('room',store,name = 'room'),
     
-    path('reservation/',reservation ,name = 'reservation'),
-    path('signup',Signup.as_view(),name = 'signup'),
-    path('login',Login.as_view(),name = 'login'),
-    path('booking', book_room_page,name = 'booking'),
-    path('booking/book', book_room,name = 'bookingroom'),
 
-    #path('reservation/success/', reservation_success, name='reservation_success'),
-#    path('staff/',staff_log_sign_page,name="staffloginpage"),
-#     path('staff/login', staff_log_sign_page,name="staffloginpage"),
-#     path('staff/signup', staff_sign_up,name="staffsignup"),
-#     path('staff/panel', panel,name="staffpanel"),
-#     path('staff/allbookings', all_bookings,name="allbookigs"),
-    path('logout/',logout,name = 'logout'),
-    path('checkout/',checkout,name = 'checkout'),
+    path('', homepage,name="homepage"),
+    path('home', homepage,name="homepage"),
+    path('about', aboutpage,name="aboutpage"),
+    path('contact', contactpage,name="contactpage"),
+    path('user', user_log_sign_page,name="userloginpage"),
+    path('user/login', user_log_sign_page,name="userloginpage"),
+    path('user/bookings', user_bookings,name="dashboard"),
+    path('user/book-room', book_room_page,name="bookroompage"),
+    path('user/book-room/book', book_room,name="bookroom"),
+    path('user/signup', user_sign_up,name="usersignup"),
+    path('staff/',staff_log_sign_page,name="staffloginpage"),
+    path('staff/login', staff_log_sign_page,name="staffloginpage"),
+    path('staff/signup', staff_sign_up,name="staffsignup"),
+    path('logout', logoutuser,name="logout"),
+    path('staff/panel', panel,name="staffpanel"),
+    path('staff/allbookings', all_bookings,name="allbookigs"),
     
+    path('staff/panel/add-new-location', add_new_location,name="addnewlocation"),
+    path('staff/panel/edit-room', edit_room),
+    path('staff/panel/add-new-room', add_new_room,name="addroom"),
+    path('staff/panel/edit-room/edit', edit_room),
+    path('staff/panel/view-room', view_room),
     
 ]
     
